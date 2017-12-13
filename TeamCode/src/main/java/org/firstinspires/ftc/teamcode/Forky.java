bbb26e708538f7738b9bf754451bf23d456c462f
DESKTOP-M8GH313\Jonah Rolfness <jonah.rolfness@gmail.com>
2017-12-12 19:08:20 -0700

    changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Forky.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Forky.java
@@ -21,20 +21,25 @@ public class Forky extends LinearOpMode
     private DcMotor motorRightLift;
     private DcMotor motorRelicArm;
     private Servo jewelServo;
+    private Servo leftServoArm;
+    private Servo rightServoArm;
+
 
     @Override
     public void runOpMode () throws InterruptedException
     {
 
         motorLeftDrive1 = hardwareMap.dcMotor.get("motorLeftDrive1");
-        motorLeftDrive2 = hardwareMap.dcMotor.get("motorLeft2");
+        motorLeftDrive2 = hardwareMap.dcMotor.get("motorLeftDrive2");
         motorRightDrive1 = hardwareMap.dcMotor.get("motorRightDrive1");
         motorRightDrive2 = hardwareMap.dcMotor.get("motorRightDrive2");
         motorStrafe = hardwareMap.dcMotor.get("motorStrafe");
         motorLeftLift = hardwareMap.dcMotor.get("motorLeftLift");
         motorRightLift = hardwareMap.dcMotor.get("motorRightLift");
         motorRelicArm = hardwareMap.dcMotor.get("motorRelicArm");
-        jewelServo = hardwareMap.servo.get ("jewelServo";
+        jewelServo = hardwareMap.servo.get ("jewelServo");
+        leftServoArm = hardwareMap.servo.get ("leftServoArm");
+        rightServoArm = hardwareMap.servo.get ("rightServoArm");
 
         motorLeftDrive1.setDirection(DcMotor.Direction.REVERSE);
         motorLeftDrive2.setDirection(DcMotor.Direction.REVERSE);
@@ -42,16 +47,16 @@ public class Forky extends LinearOpMode
 
         while(opModeIsActive())
         {
-            motorLeftDrive1.setPower(-gamepad1.left_stick_y);
-            motorLeftDrive2.setPower(-gamepad1.left_stick_y);
-            motorRightDrive1.setPower(-gamepad1.right_stick_y);jm
-            motorRightDrive2.setPower(-gamepad1.right_stick_y);
+            motorLeftDrive1.setPower(gamepad1.right_stick_y);
+            motorLeftDrive2.setPower(gamepad1.right_stick_y);
+            motorRightDrive1.setPower(gamepad1.left_stick_y);
+            motorRightDrive2.setPower(gamepad1.left_stick_y);
             motorStrafe.setPower(-gamepad1.left_stick_x);
             motorStrafe.setPower(-gamepad1.right_stick_x);
-            motorLeftLift.setPower(-gamepad2.dpad_down);
-            motorLeftLift.setPower(-gamepad2.dpad_up);
-            motorRightLift.setPower(-gamepad2.a);
-            motorRightLift.setPower(-gamepad2.y);
+            motorLeftLift.setPower(-gamepad2.left_stick_y);
+            motorRightLift.setPower(-gamepad2.left_stick_y);
+
+
 
             idle();
         }
